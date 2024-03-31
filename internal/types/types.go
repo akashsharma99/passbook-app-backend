6655453173c64d0cfe7fb91a09432ee4b5dc1806
akashsharma99 <akash.asharma.sharma11@gmail.com>
2024-03-31 23:22:28 +0530

    add passbook and transaction types

--- internal/types/types.go
+++ internal/types/types.go
@@ -18,3 +18,26 @@ type User struct {
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
+type Passbook struct {
+	PassbookID    string    `json:"passbook_id"`
+	UserID        string    `json:"user_id"`
+	BankName      string    `json:"bank_name"`
+	AccountNumber string    `json:"account_number"`
+	TotalBalance  float64   `json:"total_balance"`
+	Nickname      string    `json:"nickname"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+type Transaction struct {
+	TransactionID   string    `json:"transaction_id"`
+	Amount          float64   `json:"amount"`
+	TransactionDate time.Time `json:"transaction_date"`
+	TransactionType string    `json:"transaction_type"`
+	PartyName       string    `json:"party_name"`
+	Description     string    `json:"description"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	Tags            string    `json:"tags"`
+	PassbookID      string    `json:"passbook_id"`
+	UserID          string    `json:"user_id"`
+}
